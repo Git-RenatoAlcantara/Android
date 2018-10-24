311b6fc66d4b5f1365bd7146d981953d967e5604
Renato <renato.forjob@gmail.com>
2018-10-23 21:27:28 -0300

    Adicionado Whatsappclone / Cadastro e Login

---
+++ WhatsAppclolne/app/src/main/java/com/example/renato/whatsappclolne/activity/MainActivity.java
@@ -0,0 +1,15 @@
+package com.example.renato.whatsappclolne.activity;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.example.renato.whatsappclolne.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
