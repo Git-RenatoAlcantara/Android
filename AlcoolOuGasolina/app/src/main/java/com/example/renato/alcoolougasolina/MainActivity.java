4bcc7dc277e1f5819caed81b96c9ccad6bb812ad
Renato Alcantara <renato.forjob@gmail.com>
2019-03-07 20:02:23 -0300

    AlcoolOuGasolina calcula os dois valores exibe qual comprar

--- AlcoolOuGasolina/app/src/main/java/com/example/renato/alcoolougasolina/MainActivity.java
+++ AlcoolOuGasolina/app/src/main/java/com/example/renato/alcoolougasolina/MainActivity.java
@@ -11,6 +11,7 @@ public class MainActivity extends AppCompatActivity {
     private EditText gasolina;
     private TextView msgResultado;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
